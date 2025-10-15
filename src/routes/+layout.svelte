e072d577507ff03c3c0edddd0093e43b9591a08b
Naza <uzoukwuc@tcd.ie>
2025-10-15 18:53:37 +0100

    Update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
         export const prerender = true;
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.png" />
 </svelte:head>
 
 {@render children?.()}
